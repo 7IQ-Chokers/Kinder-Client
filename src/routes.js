faddf25f75f5327ee2e97faf8e1f291f91cf88f8
BISTArk <akshaybistagond@gmail.com>
2024-05-12 01:16:34 +0530

    bug fix

--- src/routes.js
+++ src/routes.js
@@ -21,7 +21,7 @@ const routes = [
     key: "dashboard",
     icon: <Icon fontSize="small">dashboard</Icon>,
     route: "/dashboard",
-    component: <Dashboard />,
+    component: <Tables />,
   },
   {
     type: "collapse",
