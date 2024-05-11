f5cf54146cde2d4e3edc1eba9997ce4001f8727e
BISTArk <akshaybistagond@gmail.com>
2024-05-11 15:02:59 +0530

    Routing setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Profile from './components/profile';
+import Dashboard from './components/dashboard';
+
+const router = createBrowserRouter([
+  {
+    path: "/dashboard",
+    element: Dashboard(),
+  },
+  {
+    path: "/profile",
+    element: Profile(),
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
